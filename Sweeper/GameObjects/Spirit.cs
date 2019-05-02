4c63c28f728a4392d78ba11500ac17eaed4d3827
Vincent Crowe <vincent.crowe@emishealth.com>
2019-05-02 18:44:34 +0100

    Missing file

---
+++ Sweeper/GameObjects/Spirit.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sweeper
+{
+	public class Spirit
+	{
+		private MainScene _scene;
+
+		public Spirit(int x, int y, MainScene scene)
+		{
+			_scene = scene;
+			Location = new Point(x, y);
+		}
+
+		public Point Location { get; set; }		
+
+		public void Update(GameTime time)
+		{
+			if (!_scene.PlayerMoved)
+				return;
+
+			var target = _scene.Map.GetTileAt(Location);
+			if (target.TileType == MapTileType.Hazard)
+				return;
+
+			var ToPlayer = Location.VectorTo(_scene.PlayerPosition);			
+
+			if (ToPlayer.X == 0 || ToPlayer.Y == 0)
+			{
+				_scene.Reset();
+			}						
+
+			var adjacentTiles = _scene.Map.GetAdjacentTiles(Location);
+			var bestDistance = 10000;
+			
+			foreach(var tile in adjacentTiles)
+			{
+				if(tile.Adjacents.HasValue && tile.TileType == MapTileType.Empty)
+				{
+					var distance = tile.Location.DistanceTo(_scene.PlayerPosition);
+					if (distance < bestDistance)
+					{
+						bestDistance = distance;
+						target = tile;
+					}
+				}
+			}
+
+			Location = target.Location;
+		}
+	}	
+}
